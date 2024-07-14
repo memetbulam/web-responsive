b2caaa6f2e48a4cf4a0e92ad2d1790dade5bb628
mehmetb <mehmet3bulam@gmail.com>
2024-07-15 00:06:12 +0300

    feat: search input microphone properties added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,76 @@
 "use client";
-
-import { useRef, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import CampaignsSide from "@/components/CampaignsSide";
 import SearchInput from "@/components/form-elements/SearchInput";
 import Menu from "@/components/Menu";
 import StationsSide from "@/components/StationsSide";
 import StoriesSide from "@/components/StoriesSide";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import Layout from "@/components/Layout";
 import { response } from "@/utils/constants";
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState("");
   const containerRef = useRef(null);
+  const recognitionRef = useRef<SpeechRecognition | null>(null);
+
+  const recognition = useMemo(() => {
+    if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
+      const SpeechRecognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
+      return new SpeechRecognition();
+    }
+  }, []) as SpeechRecognition;
+
+  const handleMicrophoneOpenClick = useCallback(
+    (setIsMicrophoneActive: Dispatch<SetStateAction<boolean>>) => {
+      if (
+        "SpeechRecognition" in window ||
+        "webkitSpeechRecognition" in window
+      ) {
+        if (recognitionRef.current) {
+          recognitionRef.current = recognition;
+        }
+        recognition.continuous = false;
+        recognition.interimResults = false;
+        recognition.lang = "tr-TR";
+
+        recognition.onstart = () => {
+          setIsMicrophoneActive(true);
+        };
+
+        recognition.onresult = (event) => {
+          const transcript = event.results[0][0].transcript;
+          setIsMicrophoneActive(false);
+          setSearchValue(transcript);
+        };
+
+        recognition.onerror = () => {
+          setIsMicrophoneActive(false);
+        };
+
+        recognition.start();
+      } else {
+        setIsMicrophoneActive(false);
+      }
+    },
+    [recognition]
+  );
+
+  const handleMicrophoneCloseClick = useCallback(
+    (setIsMicrophoneActive: Dispatch<SetStateAction<boolean>>) => {
+      recognition.stop();
+      setIsMicrophoneActive(false);
+    },
+    [recognition]
+  );
 
   return (
     <Layout>
@@ -35,7 +93,10 @@ export default function Home() {
         <StoriesSide storiesData={response?.data?.stories} />
         <SearchInput
           placeholder="Arama yap"
+          value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
+          handleMicrophoneOpenClick={handleMicrophoneOpenClick}
+          handleMicrophoneCloseClick={handleMicrophoneCloseClick}
         />
         <Menu menuData={response?.data?.menu} searchValue={searchValue} />
         <CampaignsSide sliderData={response?.data?.slider} />
--- src/components/form-elements/SearchInput.tsx
+++ src/components/form-elements/SearchInput.tsx
@@ -1,13 +1,17 @@
 import React, {
   ChangeEventHandler,
+  Dispatch,
   FC,
   FocusEventHandler,
+  SetStateAction,
   useState,
 } from "react";
 import { Flex } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import SearchIcon from "../icons/SearchIcon";
 import { chakraUiTheme } from "@/utils/theme";
+import MicrophoneIcon from "../icons/MicrophoneIcon";
+import MicrophoneSlashIcon from "../icons/MicrophoneSlashIcon";
 
 interface Props {
   value?: string;
@@ -15,6 +19,12 @@ interface Props {
   onFocus?: FocusEventHandler<HTMLInputElement> | undefined;
   onBlur?: FocusEventHandler<HTMLInputElement> | undefined;
   placeholder?: string;
+  handleMicrophoneOpenClick?: (
+    setIsMicrophoneActive: Dispatch<SetStateAction<boolean>>
+  ) => void;
+  handleMicrophoneCloseClick?: (
+    setIsMicrophoneActive: Dispatch<SetStateAction<boolean>>
+  ) => void;
 }
 
 const SearchInput: FC<Props> = ({
@@ -23,8 +33,11 @@ const SearchInput: FC<Props> = ({
   onFocus,
   onBlur,
   placeholder,
+  handleMicrophoneOpenClick,
+  handleMicrophoneCloseClick,
 }) => {
   const [isInputFocused, setIsInputFocused] = useState(false);
+  const [isMicrophoneActive, setIsMicrophoneActive] = useState(false);
   return (
     <Flex alignItems={"center"} gap={"16px"} position={"relative"}>
       <SearchIcon
@@ -33,7 +46,8 @@ const SearchInput: FC<Props> = ({
           height: "22px",
           position: "absolute",
           left: "12px",
-          top: "12px",
+          top: "50%",
+          transform: "translateY(-50%)",
           zIndex: 10,
           fill: isInputFocused ? chakraUiTheme.colors.activeGreen : "black",
           transition: "fill 0.3s",
@@ -41,6 +55,7 @@ const SearchInput: FC<Props> = ({
       />
       <Input
         paddingLeft={"42px"}
+        paddingRight={"50px"}
         backgroundColor={"gray.100"}
         borderRadius={"20px"}
         value={value}
@@ -62,6 +77,50 @@ const SearchInput: FC<Props> = ({
         }}
         _focus={{
           borderColor: "green.600",
+        }}
+      />
+      <MicrophoneIcon
+        style={{
+          position: "absolute",
+          right: "12px",
+          borderRadius: "50%",
+          backgroundColor: "white",
+          padding: "8px",
+          opacity: isMicrophoneActive ? 0 : 1,
+          visibility: isMicrophoneActive ? "hidden" : "visible",
+          width: "36px",
+          height: "36px",
+          zIndex: 10,
+          cursor: "pointer",
+          transition: "opacity 0.3s, visibility 0.3s",
+        }}
+        onClick={() => {
+          setIsMicrophoneActive(true);
+          if (handleMicrophoneOpenClick) {
+            handleMicrophoneOpenClick(setIsMicrophoneActive);
+          }
+        }}
+      />
+      <MicrophoneSlashIcon
+        style={{
+          position: "absolute",
+          right: "12px",
+          borderRadius: "50%",
+          backgroundColor: "white",
+          padding: "8px",
+          opacity: isMicrophoneActive ? 1 : 0,
+          visibility: isMicrophoneActive ? "visible" : "hidden",
+          width: "36px",
+          height: "36px",
+          zIndex: 10,
+          cursor: "pointer",
+          transition: "opacity 0.3s, visibility 0.3s",
+        }}
+        onClick={() => {
+          setIsMicrophoneActive(false);
+          if (handleMicrophoneCloseClick) {
+            handleMicrophoneCloseClick(setIsMicrophoneActive);
+          }
         }}
       />
     </Flex>
